81c4b4ae4d49635d3731c8a2a81e9eeb4dac3619
Klir Balista <klirbalista@gmail.com>
2025-11-07 17:08:49 +0800

    Add auth endpoints to CORS paths (register, forgot-password, etc)

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'forgot-password', 'reset-password', 'email/verification-notification'],
 
     'allowed_methods' => ['*'],
 
